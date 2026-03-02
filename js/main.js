53753e44737aff3b8576190ee385a5cef06eca3a
cynth <137558485+wancynth@users.noreply.github.com>
2026-03-01 23:58:42 -0600

    modals work now

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
 /* global nn */
-let data
+let obj
 
 async function setup () {
-  const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSmFQYcrhKAOqhQeylZmm6nODB5IkQstOmwqdtfsHF7tB6Fekonr7gTKXPNdMGl0Ili78Y3O-3NHWMQ/pub?gid=0&single=true&output=csv'
-  data = await nn.loadData(url)
-  data = nn.parseData(data)
-  console.log(data)
+  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTCohgH12G54bFzgTMbR_YTdj4knfuKNKFL2vur75wETlkFXZ7iimzkBbIIUTK9lihjmOTK07SAMX8N/pub?output=csv"
+  obj = await nn.loadData(url)
+  obj = nn.parseData(obj)
+  
+  nn.getAll('.items').forEach(setupClicks)
+  
+   console.log('Parsed data:', obj)
+  
+  console.log(obj)
   updateContent()
 }
+
 
 function updateContent () {
   const hash = window.location.hash
   const id = hash.slice(1)
   if (id) {
-    const item = data.find(i => i.id === id)
+    const item = obj.find(i => i.id === id)
     nn.get('#title').content(item.title)
     nn.get('#desc').content(item.description)
   }
 }
+
+function setupClicks (ele) {
+    ele.on('click', showModal)
+  }
+
+function showModal () {
+    const title = obj.find(i => i.id === this.id).title
+    const desc = obj.find(i => i.id === this.id).description
+    const type = obj.find(i => i.id === this.id).type
+    
+    if (type === "obj") {
+      nn.get('#modal > h2').content(title)
+      nn.get('#modal > p').content(desc)
+      nn.get('#modal').css('display', 'block')
+    }
+  }
 
 function hideModal () {
     nn.get('#modal').css('display', 'none')
@@ -27,4 +49,5 @@ function back () {
     window.location.href = window.history.back(1);
   }
 
+nn.get('#close').on('click', hideModal)
 nn.on('load', setup)
